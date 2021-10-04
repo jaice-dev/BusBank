c83881f47ceea6b57c8097d6bb818d55470049a7
Jordan <jordan.d.carlton@gmail.com>
2021-10-04 15:59:33 +0100

    Initial commit

---
+++ BusBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
