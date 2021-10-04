e69dd23a95b8106feb2c9ca9d110f731893bf22c
Jordan <jordan.d.carlton@gmail.com>
2021-10-04 16:31:45 +0100

    created RestClient and Request

--- BusBank/Program.cs
+++ BusBank/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using RestSharp;
 
 namespace BusBank
 {
@@ -6,7 +8,29 @@ namespace BusBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // https://api.digital.tfl.gov.uk/StopPoint/{id}/ArrivalDepartures
+            var softwireBusStop = "490008660N";
+            
+            var client = new RestClient("https://api.tfl.gov.uk/StopPoint");
+            var request = new RestRequest($"/{softwireBusStop}/ArrivalDepartures");
+
+            var responses = client.Get<List<Response>>(request);
+            foreach (var response in responses.Data)
+            {
+                Console.WriteLine(response);
+            }
+
+            
         }
+    }
+
+    class Response
+    {
+        public string platformName { get; set; }
+        public string destinationName { get; set; }
+        public DateTime estimatedTimeOfArrival { get; set; }
+        public DateTime estimatedTimeOfDeparture { get; set; }
+        public string departureStatus { get; set; }
+
     }
 }
