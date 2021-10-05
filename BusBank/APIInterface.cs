daf99de14a38b959602009ae36a69dc606d9edb0
Jordan <jordan.d.carlton@gmail.com>
2021-10-05 09:41:04 +0100

    refactored code

---
+++ BusBank/APIInterface.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using RestSharp;
+
+namespace BusBank
+{
+    public static class APIInterface
+    {
+        static RestClient tflclient = new RestClient("https://api.tfl.gov.uk/StopPoint");
+
+        public static float[] GetLongAndLat(string postcode)
+        {
+            var postcodeclient = new RestClient("http://api.postcodes.io/postcodes");
+            var postcoderequest = new RestRequest($"/{postcode}");
+            var postcodeResponse = postcodeclient.Get<PostcodeResponse>(postcoderequest);
+            
+            var longitude = postcodeResponse.Data.result.longitude;
+            var latitude = postcodeResponse.Data.result.latitude;
+
+            float[] locationArray = new[] {longitude, latitude};
+            return locationArray;
+        }
+
+        public static IOrderedEnumerable<StopPoints> FindNearestBusStops(float lon, float lat)
+        {
+            var nearestBusStopRequest = new RestRequest().AddQueryParameter("stopTypes", "NaptanPublicBusCoachTram")
+                .AddQueryParameter("lat", $"{lat}").AddQueryParameter("lon", $"{lon}");
+            var nearestBusStopResponses = tflclient.Get<NearestBusStopResponse>(nearestBusStopRequest);
+            return nearestBusStopResponses.Data.stopPoints.OrderBy(item => item.distance);
+        }
+
+        public static void FindNextBuses(string stopID)
+        {
+            
+        }
+    }
+}
--- BusBank/Program.cs
+++ BusBank/Program.cs
@@ -11,44 +11,49 @@ namespace BusBank
         {
             //TODO Don’t forget logging and error handling - Are you confident you’ve handled anything that can go wrong? - What happens if your user enters an invalid postcode? - What happens if there aren’t any bus stops nearby? - What happens if there aren’t any busses coming?
             //TODO The TFL API also has a 'Journey Planner'. Edit your program so that (when requested) it will also display directions on how to get to your nearest bus stops.
-            var softwireBusStop = "490008660N";
-            var postcode = "nw51tl";
-
-            var postcodeclient = new RestClient("http://api.postcodes.io/postcodes");
-            var postcoderequest = new RestRequest($"/{postcode}");
-            var postcodeResponse = postcodeclient.Get<PostcodeResponse>(postcoderequest);
-
-            var longitude = postcodeResponse.Data.result.longitude;
-            var latitude = postcodeResponse.Data.result.latitude;
-
-            var tflclient = new RestClient("https://api.tfl.gov.uk/StopPoint");
-
-            var nearestBusStopRequest = new RestRequest().AddQueryParameter("stopTypes", "NaptanPublicBusCoachTram")
-                .AddQueryParameter("lat", $"{latitude}").AddQueryParameter("lon", $"{longitude}");
-            var nearestBusStopResponses = tflclient.Get<NearestBusStopResponse>(nearestBusStopRequest);
-
-            foreach (var response in nearestBusStopResponses.Data.stopPoints)
+            var appRunning = true;
+            while (appRunning)
             {
-                Console.WriteLine(
-                    $"NaptanID: {response.naptanId}, StationID: {response.stationNaptan}, Distance: {response.distance}");
-                //TODO modify your program so that the user can supply a postcode and see the next buses at the two nearest bus stops.
+                Console.WriteLine("Welcome to BusBank. Please enter a postcode: ");
+                string userPostcode = Console.ReadLine().Replace(" ", "").ToLower();
+
+                var longAndLat= APIInterface.GetLongAndLat(userPostcode);
+                var longitude = longAndLat[0];
+                var latitude = longAndLat[1];
+                
+                Console.WriteLine($"Your location - Longitude: {longitude}, Latitude: {latitude}");
+
+                var nearestBusStops = APIInterface.FindNearestBusStops(longitude, latitude);
+                
+                foreach (var response in nearestBusStops)
+                {
+                    Console.WriteLine(
+                        $"StopNaptanID: {response.naptanId}, StopID: {response.stationNaptan}, Distance to stop: {response.distance}");
+                    //TODO modify your program so that the user can supply a postcode and see the next buses at the two nearest bus stops.
+                }
+
+
+
+
             }
             
-            var busStopRequest = new RestRequest($"/{softwireBusStop}/Arrivals");
+            // var softwireBusStop = "490008660N";
+            // var postcode = "nw51tl";
 
 
             var tflResponses = tflclient.Get<List<TFLResponse>>(busStopRequest);
             foreach (var response in tflResponses.Data.OrderBy(item => item.timeToStation).Take(5))
             {
-                Console.WriteLine(response.expectedArrival);
+                Console.WriteLine($"VehicleId: {response.vehicleId}, StationName: {response.stationName}, Direction: {response.direction}, Towards: {response.towards}, " +
+                                  $"DestinationName: {response.destinationName}, LineName: {response.lineName}, TimeToStation: {response.timeToStation}, " +
+                                  $"Expected Arrival: {response.expectedArrival}");
                 //TODO print a list of the next five buses at that stop code, with their routes, destinations, and the time until they arrive in minutes.
                 //TODO Try to ensure you're using a sensible class structure with well-named methods. Remember to commit and push your changes as you go.
 
 
             }
 
-            Console.WriteLine(
-                $"Longitude: {postcodeResponse.Data.result.longitude}, Latitude: {postcodeResponse.Data.result.latitude}");
+            
 
 
         }
@@ -62,6 +67,8 @@ namespace BusBank
         public string direction { get; set; }
         public string towards { get; set; }
         public string destinationName { get; set; }
+        public string lineName { get; set; }
+
         public int timeToStation { get; set; }
         public DateTime expectedArrival { get; set; }
 
@@ -83,7 +90,7 @@ namespace BusBank
         public List<StopPoints> stopPoints { get; set; }
     }
 
-    class StopPoints
+    public class StopPoints
     {
         public string naptanId { get; set; }
         public string stationNaptan { get; set; }
