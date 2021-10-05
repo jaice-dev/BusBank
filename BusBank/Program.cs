c14ee0b514463d0222a80097d15217e0f8d9bf3c
ChillFish8 <Harrison.Burt@softwire.com>
2021-10-05 14:19:13 +0100

    refactor code changes

--- BusBank/Program.cs
+++ BusBank/Program.cs
@@ -25,95 +25,117 @@ namespace BusBank
             while (true)
             {
                 Console.WriteLine("\nWelcome to BusBank. Please enter a postcode: ");
-                string userPostcode = Console.ReadLine().Replace(" ", "").ToLower();
+                string userPostcode = Console.ReadLine()?.Replace(" ", "").ToLower();
                 
                 if (userPostcode == "quit")
                 {
                     break;
                 }
 
-                float[] longAndLat;
-                
-                try
-                {
-                    longAndLat= APIInterface.GetLongAndLat(userPostcode);
-                }
-                catch (Exception e)
+                var location = GetLongAndLat(userPostcode);
+                if (location is null)
                 {
                     Console.WriteLine("Incorrect Postcode, please enter another.");
                     continue;
                 }
-                
-                var longitude = longAndLat[0];
-                var latitude = longAndLat[1];
-                
-                Console.WriteLine($"\nYour location - Longitude: {longitude}, Latitude: {latitude}");
 
-                var nearestBusStops = APIInterface.FindNearestBusStops(longitude, latitude).Take(2).ToArray();
-
-                if (nearestBusStops.Length == 0)
+                var result = GetBusses(location.Item1, location.Item2);
+                if (result is null)
                 {
-                    Console.WriteLine("There are no Bus Stops within a 200m radius. Please enter another postcode.");
                     continue;
                 }
 
-                var busStopCounter = 0;
-                
-                foreach (var response in nearestBusStops)
+                PromptJourneryPlanning(result.Item1, result.Item2, userPostcode);
+            }
+        }
+
+        public static Tuple<float, float> GetLongAndLat(string userPostcode)
+        {
+            float[] longAndLat;
+            try
+            {
+                longAndLat = APIInterface.GetLongAndLat(userPostcode);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            return Tuple.Create(longAndLat[0], longAndLat[1]);
+        }
+        
+        public static Tuple<int, StopPoints[]> GetBusses(float longitude, float latitude)
+        {
+            Console.WriteLine($"\nYour location - Longitude: {longitude}, Latitude: {latitude}");
+
+            var nearestBusStops = APIInterface.FindNearestBusStops(longitude, latitude).Take(2).ToArray();
+
+            if (nearestBusStops.Length == 0)
+            {
+                Console.WriteLine("There are no Bus Stops within a 200m radius. Please enter another postcode.");
+                return null;
+            }
+
+            var busStopCounter = 0;
+
+            foreach (var response in nearestBusStops)
+            {
+                busStopCounter++;
+                Console.WriteLine(
+                    $"\n{busStopCounter}) StopID: {response.naptanId}, Distance to stop: {response.distance}m\n");
+                var nextBuses = APIInterface.FindNextBuses(response.naptanId).Take(5).ToArray();
+
+                if (nextBuses.Length == 0)
                 {
-                    busStopCounter++;
+                    Console.WriteLine("There are no buses coming to this stop. Sorry!");
+                    continue;
+                }
+
+                foreach (var bus in nextBuses)
+                {
+                    TimeSpan timeDelta = DateTime.UtcNow - bus.expectedArrival;
                     Console.WriteLine(
-                        $"\n{busStopCounter}) StopID: {response.naptanId}, Distance to stop: {response.distance}m\n");
-                    var nextBuses = APIInterface.FindNextBuses(response.naptanId).Take(5).ToArray();
-
-                    if (nextBuses.Length == 0)
-                    {
-                        Console.WriteLine("There are no buses coming to this stop. Sorry!");
-                        continue;
-                    }
-                    
-                    foreach (var bus in nextBuses)
-                    {
-                        TimeSpan timeDelta = DateTime.UtcNow - bus.expectedArrival;
-                        Console.WriteLine($"    StationName: {bus.stationName}, Direction: {bus.direction}, Towards: {bus.towards}, " +
-                                     $"DestinationName: {bus.destinationName}, LineName: {bus.lineName}, " +
-                                     $"Expected Arrival: {timeDelta.Humanize()}");
-                    }
+                        $"    StationName: {bus.stationName}, Direction: {bus.direction}, Towards: {bus.towards}, " +
+                        $"DestinationName: {bus.destinationName}, LineName: {bus.lineName}, " +
+                        $"Expected Arrival: {timeDelta.Humanize()}");
                 }
-                
-                while (true)
+            }
+
+            return Tuple.Create(busStopCounter, nearestBusStops);
+        }
+
+        public static void PromptJourneryPlanning(int busStopCounter, StopPoints[] nearestBusStops, string userPostcode)
+        {
+            while (true)
+            {
+                Console.WriteLine($"\nPlan your journey to a bus stop? (Press enter to quit). Please enter a number between 1 and {busStopCounter}: ");
+                var userInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(userInput))
                 {
-                    Console.WriteLine($"\nPlan your journey to a bus stop? (Press enter to quit). Please enter a number between 1 and {busStopCounter}: ");
-                    var userInput = Console.ReadLine();
-                    if (string.IsNullOrEmpty(userInput))
-                    {
-                        break;
-                    }
-
-                    int userValue;
-                    
-                    try
-                    {
-                        userValue = int.Parse(userInput);
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Sorry, not a valid integer. Try again.");
-                        continue;
-                    }
-
-                    if (userValue < 1 || userValue > busStopCounter)
-                    {
-                        Console.WriteLine($"Sorry, that number is not in range.");
-                        continue;
-                    }
-
-                    var userNaptanId = nearestBusStops[userValue - 1].naptanId;
-                    
-                    DisplayJourney(userPostcode, userNaptanId);
-
-
+                    break;
                 }
+
+                int userValue;
+                
+                try
+                {
+                    userValue = int.Parse(userInput);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Sorry, not a valid integer. Try again.");
+                    continue;
+                }
+
+                if (userValue < 1 || userValue > busStopCounter)
+                {
+                    Console.WriteLine($"Sorry, that number is not in range.");
+                    continue;
+                }
+
+                var userNaptanId = nearestBusStops[userValue - 1].naptanId;
+                
+                DisplayJourney(userPostcode, userNaptanId);
             }
         }
 
